d261465b4b1544dc92de5b8008d7de8c81485787
AhmedAmrNabil <ahmedamr2480@gmail.com>
2025-03-26 00:07:51 +0200

    feat: add cursor pointer to search button

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -18,7 +18,7 @@ export default function SearchBar({ shadow = true, value = "" }) {
         />
         <button
           type="submit"
-          className="bg-zinc-800 text-zinc-100 px-3 py-2 rounded-full"
+          className="bg-zinc-800 text-zinc-100 px-3 py-2 rounded-full cursor-pointer"
         >
           Search
         </button>
