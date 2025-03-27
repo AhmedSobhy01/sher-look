b7d551f9b06861ea3fee1f36ca87c77fb2e8595f
OmarGamal10 <ogamal1011@gmail.com>
2025-03-27 02:11:34 +0200

    Trying tests

--- src/test/java/com/sherlook/search/query/QueryProcessorTests.java
+++ src/test/java/com/sherlook/search/query/QueryProcessorTests.java
@@ -1,3 +1,33 @@
 package com.sherlook.search.query;
 
-class QueryProcessorTests {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+import com.sherlook.search.utils.DatabaseHelper;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class QueryTests {
+  @Mock private DatabaseHelper databaseHelper;
+  @InjectMocks private QueryProcessor queryProcessor;
+
+  @Test
+  void testProcessQuery_WithThreeWords_GetsWordsIds() {
+    // Arrange
+    when(databaseHelper.getWordID(eq("machine"))).thenReturn(1);
+    when(databaseHelper.getWordID(eq("learning"))).thenReturn(2);
+    when(databaseHelper.getWordID(eq("algorithms"))).thenReturn(3);
+
+    // Act
+    List<Integer> wordsIds = queryProcessor.processQuery("machine learning algorithms");
+
+    // Assert
+    assertEquals(List.of(1, 2, 3), wordsIds);
+  }
+}
