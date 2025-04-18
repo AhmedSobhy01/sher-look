209b785c20201559a19f301109353f2d942e775c
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2025-04-18 23:37:13 +0200

    feat: relevance scoring for ranker (#5)
    * Trying tests
    
    * feat/Implemented relevance scoring with a trivial tokenizer
    
    * fix: remove database test
    
    * version 15
    
    * style: application file format
    
    * feat: add ranker relevance scoring tests

--- src/test/java/com/sherlook/search/query/QueryProcessorTests.java
+++ src/test/java/com/sherlook/search/query/QueryProcessorTests.java
@@ -1,3 +1,15 @@
 package com.sherlook.search.query;
 
-class QueryProcessorTests {}
+import static org.mockito.Mockito.*;
+
+import com.sherlook.search.utils.DatabaseHelper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class QueryProcessorTests {
+  @Mock private DatabaseHelper databaseHelper;
+  @InjectMocks private QueryProcessor queryProcessor;
+}
