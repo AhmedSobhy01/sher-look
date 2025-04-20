5a4325b3869e133fe7b4d1d0476b0b70d4d7eab2
AhmedAmrNabil <ahmedamr2480@gmail.com>
2025-04-20 20:23:56 +0200

    feat: add depth struct to urls

---
+++ src/main/java/com/sherlook/search/crawler/UrlDepthPair.java
@@ -0,0 +1,25 @@
+package com.sherlook.search.crawler;
+
+public class UrlDepthPair {
+	private String url;
+	private int depth;
+
+	public UrlDepthPair(String url, int depth) {
+		this.url = url;
+		this.depth = depth;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	@Override
+	public String toString() {
+		return "UrlDepthPair{" + "url='" + url + '\'' + ", depth=" + depth + '}';
+	}
+
+}
