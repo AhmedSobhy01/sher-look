8c80fb2586562d6a0917509e7dac7cfe6eb67dea
AhmedAmrNabil <ahmedamr2480@gmail.com>
2025-04-20 21:57:39 +0200

    feat: add equal and hash override to urlPair

--- src/main/java/com/sherlook/search/crawler/UrlDepthPair.java
+++ src/main/java/com/sherlook/search/crawler/UrlDepthPair.java
@@ -1,5 +1,7 @@
 package com.sherlook.search.crawler;
 
+import java.util.Objects;
+
 public class UrlDepthPair {
   private String url;
   private int depth;
@@ -15,6 +17,19 @@ public class UrlDepthPair {
 
   public int getDepth() {
     return depth;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    UrlDepthPair other = (UrlDepthPair) o;
+    return depth == other.depth && url.equals(other.url);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(url, depth);
   }
 
   @Override
