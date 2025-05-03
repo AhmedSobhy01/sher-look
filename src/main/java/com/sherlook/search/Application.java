53454402184efbce39fb3940499dc8736306827e
OmarGamal10 <ogamal1011@gmail.com>
2025-05-03 15:02:50 +0300

    resolve conflicts

--- src/main/java/com/sherlook/search/Application.java
+++ src/main/java/com/sherlook/search/Application.java
@@ -1,11 +1,12 @@
 package com.sherlook.search;
 
-import com.sherlook.search.crawler.Crawler;
-import com.sherlook.search.indexer.Indexer;
-import com.sherlook.search.utils.DatabaseHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import com.sherlook.search.crawler.Crawler;
+import com.sherlook.search.indexer.Indexer;
+import com.sherlook.search.utils.DatabaseHelper;
 
 @SpringBootApplication
 public class Application {
@@ -30,26 +31,46 @@ public class Application {
           System.out.println("Ready to serve");
           break;
 
-          /*
-           * case "rank":
-           * Ranker ranker = context.getBean(Ranker.class);
-           * // compute time
-           *
-           * long startTime = System.currentTimeMillis();
-           * // stop words are bottlenecks
-           * List<String> queryTerms = Arrays.asList("machine", "learning");
-           * List<RankedDocument> ranked = ranker.rank(queryTerms, false, 3, 20);
-           * long endTime = System.currentTimeMillis();
-           * long duration = endTime - startTime;
-           * System.out.println("Time taken to compute Rank: " + duration + " ms");
-           * System.out.println("Ranked documents number :" + ranked.size());
-           * System.out.println(
-           * "Ranked document first with url: "
-           * + ranked.get(2).getUrl()
-           * + " with title "
-           * + ranked.get(2).getTitle());
-           * break;
-           */
+        /*
+         * case "rank":
+         * Ranker ranker = context.getBean(Ranker.class);
+         * // compute time
+         *
+         * long startTime = System.currentTimeMillis();
+         * // stop words are bottlenecks
+         * List<String> queryTerms = Arrays.asList("machine", "learning");
+         * List<RankedDocument> ranked = ranker.rank(queryTerms, false, 3, 20);
+         * long endTime = System.currentTimeMillis();
+         * long duration = endTime - startTime;
+         * System.out.println("Time taken to compute Rank: " + duration + " ms");
+         * System.out.println("Ranked documents number :" + ranked.size());
+         * System.out.println(
+         * "Ranked document first with url: "
+         * + ranked.get(2).getUrl()
+         * + " with title "
+         * + ranked.get(2).getTitle());
+         * break;
+         */
+        /*
+         * case "rank":
+         * Ranker ranker = context.getBean(Ranker.class);
+         * // compute time
+         *
+         * long startTime = System.currentTimeMillis();
+         * // stop words are bottlenecks
+         * List<String> queryTerms = Arrays.asList("machine", "learning");
+         * List<RankedDocument> ranked = ranker.rank(queryTerms, false, 3, 20);
+         * long endTime = System.currentTimeMillis();
+         * long duration = endTime - startTime;
+         * System.out.println("Time taken to compute Rank: " + duration + " ms");
+         * System.out.println("Ranked documents number :" + ranked.size());
+         * System.out.println(
+         * "Ranked document first with url: "
+         * + ranked.get(2).getUrl()
+         * + " with title "
+         * + ranked.get(2).getTitle());
+         * break;
+         */
 
         default:
           System.out.println("Usage: java -jar search-engine.jar [crawl|index|serve]");
